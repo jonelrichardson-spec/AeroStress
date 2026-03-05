e078ceb55a1694b352c765cb663fa252c48017ae
jonelrichardson-spec <jonel.richardson@pursuit.org>
2026-03-04 22:06:28 -0500

    Add new origins to CORS middleware
    adding FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,8 +11,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
-    allow_credentials=True,
+allow_origins=[
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://aerostress.onrender.com",
+    "aerostress-d9v6w5d04-papes-projects-f59f593c.vercel.app",
+],    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
